4910872a98cdd2ded96ff048907fde5f25c4bf82
Ashish <feather.catilizr@gmail.com>
2021-11-28 18:59:20 +0530

    master

---
+++ Whole Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i;
+
+    i=0;
+
+    printf("The first 10 whole number");
+    for (i=0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;    
+}
