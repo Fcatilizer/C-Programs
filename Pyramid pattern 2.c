a57560166cc71c28979ac8003953f26a2442dba2
Ashish <feather.catilizr@gmail.com>
2021-12-02 15:31:40 +0530

    master

--- Pyramid pattern 2.c
+++ Pyramid pattern 2.c
@@ -5,7 +5,7 @@ int main(){
     {
         for ( j = 1; j <= i; j++)
         {
-            printf("*", j);
+            printf("*");
         }
         printf("\n");
     }
---
+++ Pyramid pattern 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int i, j, k, l;
+    for ( i = 1; i <= j; i++)
+    {
+        for ( j = 1; j <= 3; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
